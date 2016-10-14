33fc8d67855951bf677d00d61dc06f3f49861b96
Kevin Brightwell <kevin.brightwell2@gmail.com>
2016-10-13 20:59:41 -0400

    Add message to canvas

--- src/client/public/javascripts/client.js
+++ src/client/public/javascripts/client.js
@@ -10,4 +10,11 @@ $(function () {
     window.location.href = "/";
   });
 
+  var $canvas = $("#canvas");
+  var ctx = $canvas[0].getContext('2d');
+  ctx.font = "48px Helvetica";
+  ctx.fillText("Downloading simulation...", $canvas.width()/2. - 225, $canvas.height()/2. - 24);
+
+  console.log("w=", $canvas.width(), "h=", $canvas.height());
+
 });
